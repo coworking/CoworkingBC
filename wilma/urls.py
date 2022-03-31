8b5b059170d058ffa9bd49695a6cf533dac4fba4
Jacob Sayles <jsayles@gmail.com>
2022-03-31 14:33:51 -0700

    fixed url

--- wilma/urls.py
+++ wilma/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
+from django.http import HttpResponse
 from django.contrib import admin
-from django.views.generic.base import RedirectView
 
 from wilma import views
 
 
 urlpatterns = [
-    path('favicon.ico', RedirectView.as_view(url='/static/img/favicon-32x32.png', permanent=True)),
     path('robots.txt', lambda r: HttpResponse("User-agent: *\nDisallow: /", content_type="text/plain")),
 
     path('', views.home, name='home'),
